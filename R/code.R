ef5eedfe207f1389e699dd38605058f74f9a056d
max607 <maxschneider2000@pm.me>
2023-01-30 17:12:17 +0200

    newton raphson and clean code

--- R/code.R
+++ R/code.R
@@ -1,15 +1,24 @@
 # Data ---------------------------------------------------------------------------------------------
 
 vec_sample <- read.table("data.txt") %>% unlist() %>% unname()
+y_bar <- mean(vec_sample)
 
 # Functions ----------------------------------------------------------------------------------------
 
-my_df <- function(x, theta, log = FALSE) {
-  theta^2 / (theta + 1) * (1 + x) * exp(-theta * x)
+f_y <- function(y, theta, log = FALSE) {
+  theta^2 / (theta + 1) * (1 + y) * exp(-theta * y)
 }
 
-ml_estimation <- function(theta, x_bar) {
-  (theta + 2) / (theta * (theta + 1)) - x_bar
+loglik <- function(theta, y_bar) {
+  2 * log(theta) - log(theta + 1) - y_bar * theta
+}
+
+d_loglik <- function(theta, y_bar) {
+  2 * theta^(-1) - (theta + 1)^(-1) - y_bar
+}
+
+dd_loglik <- function(theta) {
+  - 2 * theta^(-2) + (theta + 1)^(-2)
 }
 
 # Plots --------------------------------------------------------------------------------------------
@@ -27,7 +36,7 @@ ml_estimation <- function(theta, x_bar) {
 # for (i in exp(seq(log(0.01), log(100), length = fps * seconds))) {
 #   print(
 #     p +
-#       geom_function(fun = my_df, args = list(theta = i), n = 501) +
+#       geom_function(fun = f_y, args = list(theta = i), n = 501) +
 #       labs(title = paste("theta:", i))
 #   )
 # }
@@ -38,51 +47,79 @@ ml_estimation <- function(theta, x_bar) {
 
 ## ML estimator ------------------------------------------------------------------------------------
 
-ggplot(data.table(x = 1)) +
-  geom_function(fun = function(theta) (theta + 2) / (theta * (theta + 1)), n = 501) +
-  geom_hline(yintercept = mean(vec_sample)) +
+p1 <- ggplot() +
+  geom_function(fun = loglik, args = list(y_bar = y_bar), n = 501) +
   xlim(0, 10)
+
+p2 <- ggplot() +
+  geom_function(fun = d_loglik, args = list(y_bar = y_bar), n = 501) +
+  xlim(0.1, 10)
 
 # Estimation ---------------------------------------------------------------------------------------
 
-## Using R functions -------------------------------------------------------------------------------
+## Straight optimization ---------------------------------------------------------------------------
 
-my_estimation <- function(vec_data) {  # TODO: replace with gradient acent
-  uniroot(ml_estimation, x_bar = mean(vec_data), interval = c(0, 10),
+estimation_uniroot <- function(vec_data) {
+  uniroot(d_loglik, y_bar = mean(vec_data), interval = c(0, 10),
           tol = .Machine$double.eps^0.5)$root
 }
 
-theta_hat <- my_estimation(vec_sample)
+theta_hat1 <- estimation_uniroot(vec_sample)
+
+## Newton-Raphson ----------------------------------------------------------------------------------
+
+newton_raphson <- function(theta0 = 1, dl = d_loglik, ddl = dd_loglik, ...) {
+  while (abs(round(step <- dl(theta0, ...) / ddl(theta0), 8)) > 0) {
+    theta0 <- theta0 - step
+  }
+  theta0
+}
+
+theta_hat2 <- newton_raphson(y_bar = y_bar)
+
+## Plot --------------------------------------------------------------------------------------------
+
+p3 <- p1 +
+  geom_vline(xintercept = theta_hat1) +
+  geom_vline(xintercept = theta_hat2, color = "red") +
+  geom_hline(yintercept = loglik(theta_hat2, y_bar))
+
+# microbenchmark::microbenchmark(estimation_uniroot(vec_sample),
+#                                newton_raphson(y_bar = y_bar),
+#                                times = 1e4)
 
 # Parametric bootstrap -----------------------------------------------------------------------------
 
-## RNG1 --------------------------------------------------------------------------------------------
-
 my_exp <- function(n, theta) -log(runif(n)) / theta
+
+## RNG1 --------------------------------------------------------------------------------------------
 
 my_gamma <- function(shape, theta) sum(my_exp(shape, theta))
 
-my_sampler <- function(n, theta) {
+sampler1 <- function(n, theta) {
   shape <- ifelse(runif(n, 0, theta + 1) < 1, 2, 1)
   sapply(shape, my_gamma, theta = theta)
 }
 
 ## RNG2 --------------------------------------------------------------------------------------------
 
-my_sampler2 <- function(n, theta) {
+sampler2 <- function(n, theta) {
   vec_res <- my_exp(n, theta)
   vec_shape2 <- my_exp(sum(runif(n, 0, theta + 1) < 1), theta)
   vec_res + c(vec_shape2, rep(0, length(vec_res) - length(vec_shape2)))
 }
 
-# microbenchmark::microbenchmark(my_sampler(150, theta_hat), my_sampler2(150, theta_hat),
+# microbenchmark::microbenchmark(sampler1(150, theta_hat), sampler2(150, theta_hat),
 #                                times = 1e4)
 
 ## Standard error ----------------------------------------------------------------------------------
 
 set.seed(1)
-ls_bss <- replicate(1e4, my_sampler2(n = length(vec_sample), theta = theta_hat),
-                    simplify = FALSE)
-vec_theta_hat <- sapply(ls_bss, my_estimation)
-sd(vec_theta_hat)
+boot <- replicate(1e4, {
+  sample_par <- sampler2(n = length(vec_sample), theta = theta_hat2)
+  newton_raphson(y_bar = mean(sample_par))
+})
+boot.mean <- mean(boot)
+boot.se <- sqrt(mean((boot - boot.mean)^2))
+boot.bias <- boot.mean - theta_hat2
 
